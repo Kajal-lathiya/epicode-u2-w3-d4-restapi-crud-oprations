30822b9ae3ea9e3d6d087aeed573b18431bf1b63
Kajal lathiya <kajalbodarya77@gmail.com>
2022-11-10 21:51:54 +0530

    code clean

--- Home/script.js
+++ Home/script.js
@@ -53,8 +53,5 @@ renderProducts = (listOfProducts) => {
 
 window.onload = async() => {
     const products = await getProducts();
-    //     <div class="spinner-border text-success" role="status">
-    //   <span class="sr-only">Loading...</span>
-    // </div>
     renderProducts(products);
 };
